ab4e608055e1b14de7d59cdd86fc1f7e01159749
Bam-j <juhyun19981129@gmail.com>
2026-03-16 20:08:36 +0900

    chore: Swagger 의존성 및 설정 추가

---
+++ backend/src/main/java/com/auction/backend/global/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.auction.backend.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auction System API")
+                        .description("경매 시스템 백엔드 API 명세서입니다.")
+                        .version("v1.0.0"));
+    }
+}
